695d1fd6c1a806f5a2e2f53eac6b03402681daff
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-03-12 21:47:16 -0500

    primer despliegue

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
-from .views import paciente_list, paciente_historial
+from .views import home, paciente_list, paciente_historial
 
 urlpatterns = [
-    path('', paciente_list, name='paciente_list'),  
+    path('', home, name='home'), 
+    path('pacientes/', paciente_list, name='paciente_list'),
     path('paciente/historial/<int:paciente_id>/', paciente_historial, name='paciente_historial'),
 ]
