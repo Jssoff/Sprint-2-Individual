ec4adf476ec47b69bdc8c9e7c1ad41fc2f36a969
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-04-02 12:06:37 +0000

    Procesar Imagen

--- imagen/models.py
+++ imagen/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from pacientes.models import Paciente
 
 class ImagenMedica(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=255)
     archivo = models.FileField(upload_to='imagenes/')
     fecha_carga = models.DateTimeField(auto_now_add=True)
