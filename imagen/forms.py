a4bb6201ad8adec48a4f9eed6a38a16310676b4d
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-04-02 21:36:44 -0500

    paciente

--- imagen/forms.py
+++ imagen/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import ImagenMedica
+from .models import ImagenMedica, Paciente
 
 class ImagenMedicaForm(forms.ModelForm):
     class Meta:
         model = ImagenMedica
-        fields = ['nombre', 'archivo']
+        fields = ['nombre', 'archivo', 'paciente']
+    def clean_paciente(self):
+        paciente = self.cleaned_data.get('paciente')
+        if not Paciente.objects.filter(id=paciente.id).exists():
+            raise forms.ValidationError('El paciente seleccionado no existe.')
+        return paciente
