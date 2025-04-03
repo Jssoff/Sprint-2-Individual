ab95255cce7cd0d76ac09e7f2e6fb3289fc657be
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-04-02 22:03:11 -0500

    f

--- imagen/urls.py
+++ imagen/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('cargar/', views.cargar_imagen, name='cargar_imagen'),
     path('reducir/<int:paciente_id>/', views.reducir_imagen, name='reducir_imagen'),
+    path('descargar/<int:paciente_id>/', views.descargar_imagen, name='descargar_imagen'),  # Nueva ruta
 ]
