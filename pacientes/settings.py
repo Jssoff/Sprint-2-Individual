656ea237f458c825d1f9d95e45aec3d5af81ca09
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-04-26 06:32:57 -0500

    csrf config

--- pacientes/settings.py
+++ pacientes/settings.py
@@ -44,11 +44,6 @@ INSTALLED_APPS = [
     'imagen',
 ]
 
-# Desactivar la protección CSRF completamente (solo para pruebas)
-CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = ['*']
-CSRF_COOKIE_HTTPONLY = False
-CSRF_USE_SESSIONS = False
 
 
 
@@ -56,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
