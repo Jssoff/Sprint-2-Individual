9db04a17df2b972fcd1329298c9fbc6da2578a75
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-02 06:21:54 -0500

    ayuda

--- diagnostico/urls.py
+++ diagnostico/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('visualizar_paciente/<int:paciente_id>/', views.visualizar_imagenes_paciente, name='visualizar_imagenes_paciente'),
     path('seleccionar_paciente/', views.seleccionar_paciente, name='seleccionar_paciente'),
