1faa36a0568eeaf3c49441afe7560c9d5bde63e7
Jessica Garay <141933661+Jssoff@users.noreply.github.com>
2025-05-03 14:18:00 -0500

    Update urls.py

--- diagnostico/urls.py
+++ diagnostico/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('visualizar_paciente/<int:paciente_id>/', views.visualizar_imagenes_paciente, name='visualizar_imagenes_paciente'),
     path('seleccionar_paciente/', views.seleccionar_paciente, name='seleccionar_paciente'),
     path('imagen/<int:imagen_id>/', views.visualizar_imagen, name='visualizar_imagen'),
-    path('health/', views.healthCheck, name= 'health'),	
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
