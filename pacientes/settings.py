815efed1b80cf3de79b59e1d0c7e5aa21bbe92c1
Jssoff <js.garay@uniandes.com>
2025-05-03 18:32:26 -0500

    Trust

--- pacientes/settings.py
+++ pacientes/settings.py
@@ -104,3 +104,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://34.27.176.71:8000']
+
