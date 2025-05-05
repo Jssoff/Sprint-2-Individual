c3786c3f073bf298efd8fb1e6b24213e32a63daf
Jssoff <js.garay@uniandes.com>
2025-05-05 09:29:40 -0500

    crear historia

--- historias_clinicas/urls.py
+++ historias_clinicas/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import historia_list, historia_detail, historia_create
+from .views import historia_list, historia_detail, historia_create, crear_historia
 
 urlpatterns = [
     path('api/historias/', historia_list, name='historia_list'),
     path('api/historia/<int:historia_id>/', historia_detail, name='historia_detail'),
-    path('api/historia/crear/','crear_historia', name='crear_historia'),
+    path('api/historia/crear/<int:paciente_id>/', crear_historia, name='crear_historia'),
 ]
