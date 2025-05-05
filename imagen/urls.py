d2e487faba8e15159fda2556274c7c36d945e7be
Jssoff <js.garay@uniandes.com>
2025-05-04 19:01:14 -0500

    creo q ya

--- imagen/urls.py
+++ imagen/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('reducir/<int:paciente_id>/', views.reducir_imagen, name='reducir_imagen'),
     path('descargar/<int:paciente_id>/', views.descargar_imagen, name='descargar_imagen'),  
     path('imagen/<int:imagen_id>/', views.visualizar_imagen, name='visualizar_imagen'),
-path('visualizar_paciente/<int:paciente_id>/', views.visualizar_imagenes_paciente, name='visualizar_imagenes_paciente'),
+    path('visualizar_paciente/<int:paciente_id>/', views.visualizar_imagenes_paciente, name='visualizar_imagenes_paciente'),
     path('imagen/<int:imagen_id>/', views.visualizar_imagen, name='visualizar_imagen'),
 ]
