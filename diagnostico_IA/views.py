7f97d0658c3167bc5a14da492e0526505a69b320
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-23 12:42:38 -0500

    ia no definida

--- diagnostico_IA/views.py
+++ diagnostico_IA/views.py
@@ -1,1 +1,27 @@
 
+from django.shortcuts import render
+import torch
+import nibabel as nib
+import numpy as np
+from django.conf import settings
+from .modelo_epilepsia import EpilepsyCNN
+from pacientes import *
+import tempfile
+import os
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
+def formulario_diagnostico(request):
+    return render(request, 'diagnostico_IA/formulario.html')
+
+
+
+
+
+def predecir_epilepsia(request):{
+
+}
+   
+
+
