66db8457f8bbd1397151ae708620a0990bf43c26
Stiff0126-aj <m.londonot@uniandes.edu.co>
2025-05-26 20:27:47 -0500

    seguridad

--- sprint2individual/views.py
+++ sprint2individual/views.py
@@ -5,8 +5,8 @@ from sprint2individual.login import autenticacion
 
 @autenticacion
 def home(request):
-    return render(request, 'sprint/home.html')
+    return render(request, 'Paciente/home.html')
 
-
+@autenticacion
 def formulario_remoto(request):
     return redirect('https://diagnostico-ia-594687202111.us-central1.run.app/ia/formulario_predecir/')
