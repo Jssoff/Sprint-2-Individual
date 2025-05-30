f195ba1d04dae9dcc796cb975b83d3b152fc4e8a
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-29 22:03:33 -0500

    cargar

--- imagen/urls.py
+++ imagen/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
 urlpatterns = [
-    path('cargar/', TemplateView.as_view(template_name='imagen/cargar_imagen.html'), name='cargar_imagen'),
+    path('cargar/', RedirectView.as_view(url='/imagen/upload/', permanent=False), name='cargar_imagen'),
     path('dj-cargar/', views.cargar_imagen, name='dj_cargar_imagen'),
     path('dj-reducir/<int:paciente_id>/', views.reducir_imagen, name='dj_reducir_imagen'),
     path('dj-descargar/<int:paciente_id>/', views.descargar_imagen, name='dj_descargar_imagen'),  
